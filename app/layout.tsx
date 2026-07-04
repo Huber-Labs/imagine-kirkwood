644116fd4d1deca153461559834fb450cc386894
Dave Huber <byhuber@gmail.com>
2026-07-04 14:13:14 -0400

    Refine map visuals

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       lang="en"
       className={`${instrumentSerif.variable} ${inter.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-row flex-wrap">{children}</body>
     </html>
   );
 }
